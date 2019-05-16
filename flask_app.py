e70b31c304c6d68f6328d5a61c2c57b1697fadc1
Carter <carter.luck@djusdk12ca.org>
2019-05-16 08:49:24 -0700

    add gus code

--- flask_app.py
+++ flask_app.py
@@ -1,30 +1,30 @@
 from flask import Flask, render_template, request
 from random import randint
+import random
 app = Flask(__name__)
 @app.route('/')
 def main():
 	return render_template("home.html")
-	
-@app.route('/add',methods=["GET","POST"])
+
+@app.route('/add', methods=['GET','POST'])
 def add():
-	correct = 0
-	r = "1; /add"
-	if request.method == "GET":
-		o = "+"
-		n1,n2 = randint(-100,100),randint(-100,100)
-		calcanswer = n1 + n2
-		print([o,n1,n2,calcanswer])
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,calcanswer=calcanswer,correct=correct,r=r)
-	else: 
-		calcanswer = request.form['calcanswer']
-		useranswer = request.form['useranswer']
-		n1 = request.form['n1']
-		n2 = request.form['n2']
-		o = request.form['o']
-		print([calcanswer,useranswer,n1,n2,o])
-		if calcanswer == useranswer: correct = 1
-		else: correct = 2
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,correct=correct,useranswer=useranswer,r=r)
+    correct = 0
+    if request.method=='GET':
+        a = random.randint(1,10)
+        b = random.randint(1,5)
+        calcanswer = a**b
+        useranswer=0
+        return render_template('exponents.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
+    else:
+        a = request.form['a']
+        b = request.form['b']
+        calcanswer = int(a)**int(b)
+        useranswer = request.form['useranswer']
+        if request.form['useranswer'].strip() == request.form['calcanswer'].strip():
+            correct=1
+        else:
+            correct=-1
+        return render_template('multiplytwo.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
 		
 @app.route('/subtract', methods=["GET","POST"])
 def subtract():
@@ -47,26 +47,25 @@ def subtract():
 		else: correct = 2
 		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,correct=correct,useranswer=useranswer,r=r)
 
-@app.route('/multiply', methods=["GET","POST"])
-def multiply():
-	correct = 0
-	r = "1; /multiply"
-	if request.method == "GET":
-		o = "x"
-		n1,n2 = randint(-100,100),randint(-100,100)
-		calcanswer = n1 * n2
-		print([o,n1,n2,calcanswer])
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,calcanswer=calcanswer,correct=correct,r=r)
-	else: 
-		calcanswer = request.form['calcanswer']
-		useranswer = request.form['useranswer']
-		n1 = request.form['n1']
-		n2 = request.form['n2']
-		o = request.form['o']
-		print([calcanswer,useranswer,n1,n2,o])
-		if calcanswer == useranswer: correct = 1
-		else: correct = 2
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,correct=correct,useranswer=useranswer,r=r)
+@app.route('/multiplytwo', methods=['GET','POST'])
+def multiplytwo():
+    correct = 0
+    if request.method=='GET':
+        a = random.randint(1,25)
+        b = random.randint(1,25)
+        calcanswer = a*b
+        useranswer=0
+        return render_template('multiplytwo.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
+    else:
+        a = request.form['a']
+        b = request.form['b']
+        calcanswer = int(a)*int(b)
+        useranswer = request.form['useranswer']
+        if request.form['useranswer'].strip() == request.form['calcanswer'].strip():
+            correct=1
+        else:
+            correct=-1
+        return render_template('multiplytwo.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
 
 @app.route('/divide', methods=["GET","POST"])
 def divide():
@@ -89,24 +88,67 @@ def divide():
 		else: correct = 2
 		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,correct=correct,useranswer=useranswer,r=r)
 
-@app.route('/exponent', methods=["GET","POST"])
-def exponent():
-	correct = 0
-	r = "1; /exponent"
-	if request.method == "GET":
-		o = "^"
-		n1,n2 = randint(-100,100),randint(-100,100)
-		calcanswer = n1 ** n2
-		print([o,n1,n2,calcanswer])
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,calcanswer=calcanswer,correct=correct,r=r)
-	else: 
-		calcanswer = request.form['calcanswer']
-		useranswer = request.form['useranswer']
-		n1 = request.form['n1']
-		n2 = request.form['n2']
-		o = request.form['o']
-		print([calcanswer,useranswer,n1,n2,o])
-		if calcanswer == useranswer: correct = 1
-		else: correct = 2
-		return render_template("thetemplate.html",o=o,n1=n1,n2=n2,correct=correct,useranswer=useranswer,r=r)
+@app.route('/exponents', methods=['GET','POST'])
+def exponents():
+    correct = 0
+    if request.method=='GET':
+        a = random.randint(1,10)
+        b = random.randint(1,5)
+        calcanswer = a**b
+        useranswer=0
+        return render_template('exponents.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
+    else:
+        a = request.form['a']
+        b = request.form['b']
+        calcanswer = int(a)**int(b)
+        useranswer = request.form['useranswer']
+        if request.form['useranswer'].strip() == request.form['calcanswer'].strip():
+            correct=1
+        else:
+            correct=-1
+        return render_template('exponents.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
 		
+@app.route('/pythagorean/<num1>/<num2>/<a2>/<b2>/<c>/<x>/<useranswer>/')
+def pythagorean(num1,num2,a2,b2,x,c,useranswer):
+    num1=random.randint(1,10)
+    num2=random.randint(1,10)
+    a2=((num1)**2)
+    b2=((num2)**2)
+    total = (a2+b2)
+    x=math.sqrt(total)
+    c=round(x,2)
+    print('A= ',num1,'B= ',num2,c)
+    while True:
+        useranswer=input('What is C? ')
+        if float(useranswer)==float(c):
+            print('That is the correct answer')
+            break;
+        else:
+            print('That is not the correct answer, try again.')
+    return render_template(useranswer,num1,num2,total,c)
+
+@app.route('/addfractions', methods=['GET','POST'])
+def addfractions():
+    correct = 0
+    if request.method=='GET':
+        a = random.randint(1,10)
+        b = random.randint(1,10)
+        c = random.randint(1,10)
+        d = random.randint(1,10)
+        calcanswer = (a/b)+(c/d)
+        useranswer=0
+        return render_template('exponents.html',a=a,b=b,calcanswer=calcanswer,useranswer=useranswer,correct=correct)
+    else:
+        a = request.form['a']
+        b = request.form['b']
+        c = request.form['c']
+        d = request.form['d']
+        calcanswer = (int(a)/int(b))+(int(c)/int(d))
+        useranswer1 = request.form['useranswer1']
+        useranswer2 = request.form['useranswer2']
+        useranswerfull = useranswer1/useranswer2
+        if useranswerfull == request.form['calcanswer'].strip():
+            correct=1
+        else:
+            correct=-1
+        return render_template('exponents.html',a=a,b=b,calcanswer=calcanswer,useranswerfull=useranswer,correct=correct)
